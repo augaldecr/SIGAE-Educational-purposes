2d52a8d24d3dfba750d984851729ce536a3903ef
Alonso Ugalde <augaldecr@hotmail.com>
2020-02-11 11:03:04 -0600

    20200211

---
+++ SIGAE.Padron/Data/Persona.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SIGAE.Padron.Data
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string Cedula { get; set; }
+        public string Genero { get; set; }
+        public string Apellido1 { get; set; }
+        public string Apellido2 { get; set; }
+        public string Nombre { get; set; }
+    }
+}
